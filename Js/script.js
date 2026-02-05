53e3833f7851b4bab6fcb82927ab39b4056cab20
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-05 11:52:26 +0600

    anothe

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+const inputFeild = document.getElementById("inputFeild");
+console.log(inputFeild.value);
+const tag = document.getElementsByTagName('h1')
+// console.log(tag.value);
+
+// Anothe js code start here:-
+const addItmes = document.getElementById("addItems")
